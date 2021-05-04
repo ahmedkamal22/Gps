9614a3aa1663234da4b86af3051fcf0077de3443
ahmedkamal22 <ak0850000@gmail.com>
2021-05-04 13:27:00 +0300

    Added google play services dependency in the gradle Get live location

--- app/src/main/java/com/kamal/gps/MainActivity.kt
+++ app/src/main/java/com/kamal/gps/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.kamal.gps
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.ActivityManager
 import android.content.DialogInterface
+import android.content.IntentSender
 import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
+import android.util.Log
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.islami.base.BaseActivity
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.Task
 
 class MainActivity : BaseActivity() {
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         if(isLocationPermissionGranted())
         {
             showUserLocation()
@@ -56,9 +66,69 @@ class MainActivity : BaseActivity() {
         }
     }
 
+    @SuppressLint("MissingPermission")
     private fun showUserLocation() {
+        /* get Location once
+        fusedLocationClient.lastLocation.addOnSuccessListener {
+            location:Location? ->
+            if(location!=null)
+            {
+                Log.e("latitude","${location.latitude}")
+                Log.e("longitude","${location.longitude}")
+            }
+        }*/
+        veriftLocationFromSettings()
         makeToast("Accessing User Location Successfully!!")
     }
+    val locationRequest = LocationRequest.create()?.apply {
+        interval = 10000
+        fastestInterval = 5000
+        priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+    }
+    private fun veriftLocationFromSettings() {
+        val builder = LocationSettingsRequest.Builder()
+
+        val client: SettingsClient = LocationServices.getSettingsClient(this)
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+        task.addOnSuccessListener {
+            startUserLocationTracking()
+        }
+        task.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException){
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    exception.startResolutionForResult(this@MainActivity,
+                            REQUEST_CHECK_SETTINGS)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                }
+
+            }
+        }
+    }
+    val REQUEST_CHECK_SETTINGS = 200
+    val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            super.onLocationResult(locationResult)
+            for (location in locationResult.locations) {
+                Log.e("latitude","${location.latitude}")
+                Log.e("longitude","${location.longitude}")
+            }
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+    @SuppressLint("MissingPermission")
+    private fun startUserLocationTracking() {
+        fusedLocationClient.requestLocationUpdates(locationRequest,locationCallback, Looper.getMainLooper())
+    }
+
 
     private fun isLocationPermissionGranted(): Boolean {
         return ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
