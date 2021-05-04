54df4df1af26b4433846a5e363a8fbb446e9cc83
ahmedkamal22 <ak0850000@gmail.com>
2021-05-04 04:16:48 +0300

    In this branch I Added base package to use dialogue and toast Added location permission in the manifest file Added Location permission to MainActivity

--- app/src/main/java/com/kamal/gps/MainActivity.kt
+++ app/src/main/java/com/kamal/gps/MainActivity.kt
@@ -1,11 +1,66 @@
 package com.kamal.gps
 
+import android.Manifest
+import android.app.ActivityManager
+import android.content.DialogInterface
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.islami.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if(isLocationPermissionGranted())
+        {
+            showUserLocation()
+        }
+        else
+        {
+            requestLocationPermission()
+        }
+    }
+    val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                // Permission is granted. Continue the action or workflow in your
+                // app.
+                showUserLocation()
+            } else {
+                // Explain to the user that the feature is unavailable because the
+                // features requires a permission that the user has denied. At the
+                // same time, respect the user's decision. Don't link to system
+                // settings in an effort to convince the user to change their
+                // decision.
+                makeToast("We are sorry we can't bring to yours best restaurant unless you give us the permission")
+            }
+        }
+    private fun requestLocationPermission() {
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION))
+        {
+            showDialig(title = "Mandatory Permission",message = "PLease give us the permission to access your location that we can bring to yours best restaurant",
+            posActionName = "Certainly",posAction = DialogInterface.OnClickListener { dialog, which ->
+                    requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                    dialog.dismiss()
+                },negActionName = "No Thanks",negAction = DialogInterface.OnClickListener { dialog, which -> dialog.dismiss() })
+        }
+        else
+        {
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    private fun showUserLocation() {
+        makeToast("Accessing User Location Successfully!!")
+    }
+
+    private fun isLocationPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
     }
 }
